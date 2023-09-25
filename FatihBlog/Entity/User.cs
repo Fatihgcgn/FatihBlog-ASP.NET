fc3400f83cdb80f378962d2264c7894e7de0cdbb
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-25 18:30:23 +0300

    Entities
    Entities

---
+++ FatihBlog/Entity/User.cs
@@ -0,0 +1,9 @@
+﻿namespace FatihBlog.Entity;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string? UserName { get; set; }
+    public List<Post> Post { get; set; } = new List<Post>();
+    public List<Comment> Comments { get; set; } = new List<Comment>();
+}
