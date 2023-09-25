730c5e074175cf19c3aa90a2368fa61399627d95
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-25 18:47:23 +0300

    DbContext and sqllite efcore setup
    DbContext and sqllite efcore setup

---
+++ FatihBlog/Data/Concrete/EfCore/FatihBlogContext.cs
@@ -0,0 +1,17 @@
+using FatihBlog.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FatihBlog.Data.Concrete.EfCore
+{
+    public class FatihBlogContext:DbContext
+    {
+        public FatihBlogContext(DbContextOptions<FatihBlogContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        public DbSet<User> Users => Set<User>();
+    }
+}
