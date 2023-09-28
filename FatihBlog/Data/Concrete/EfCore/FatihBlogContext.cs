a5f9b8bcfc17e85b4e711e856776305935072f03
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-09-28 16:36:37 +0300

    FatihBlogContext and connection String
    FatihBlogContext and connection String

--- FatihBlog/Data/Concrete/EfCore/FatihBlogContext.cs
+++ FatihBlog/Data/Concrete/EfCore/FatihBlogContext.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FatihBlog.Data.Concrete.EfCore
 {
-    public class FatihBlogContext:DbContext
+    public class FatihBlogContext : DbContext
     {
-        public FatihBlogContext(DbContextOptions<FatihBlogContext> options):base(options)
+        public FatihBlogContext(DbContextOptions<FatihBlogContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Post> Posts => Set<Post>();
         public DbSet<Comment> Comments => Set<Comment>();
