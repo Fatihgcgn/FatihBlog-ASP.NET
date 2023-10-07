bb5cbcd69ecda40fc17c049b228d30bbc644e1c5
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-10-07 19:36:33 +0300

    New Posts
    New Posts

--- FatihBlog/ViewComponents/TagsMenu.cs
+++ FatihBlog/ViewComponents/TagsMenu.cs
@@ -1,5 +1,6 @@
 using FatihBlog.Data.Abstract;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace FatihBlog.ViewComponents
@@ -12,9 +13,9 @@ namespace FatihBlog.ViewComponents
             _tagRepository = tagRepository;
         }
 
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View(_tagRepository.Tags.ToList());
+            return View(await _tagRepository.Tags.ToListAsync());
         }
     }
 }
